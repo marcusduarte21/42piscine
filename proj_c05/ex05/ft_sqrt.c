b28decca0f8d6b9f458d9b44985f02ea6b440d00
Marcus Duarte <marcus@DESKTOP-A02T9Q4>
2024-01-27 01:32:13 +0100

    ok

--- proj_c05/ex05/ft_sqrt.c
+++ proj_c05/ex05/ft_sqrt.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 
-int ft_sqrt(int nb)
+float ft_sqrt(int nb)
 {
-	int	i;
+	float	i;
 
 	i = 0;
-	while (i * i != nb)
-	{
+	if (nb <= 0)
+		return (0);
+	while (i * i <= nb)
 		i++;
+	if (i * i == nb)
+		return (i);
+	else
+	{
+		i--;
+		while(i * i < nb)
+		{
+			i = i + 0.1;
+		}
+		i = i - 0.1;
+		while(i * i < nb)
+			i = i + 0.000001;
 	}
 	return (i);
+
 
 }
 
 int	main(void)
 {
-	int num;
-	num = ft_sqrt(25);
-	printf("%d\n", num);
+	float num;
+	num = ft_sqrt(30);
+	printf("%f\n", num);
 }
